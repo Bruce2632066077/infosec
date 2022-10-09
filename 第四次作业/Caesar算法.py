c5f47cbd9599aec80ec4c6c28edd0af6cba08c5d
3120005008 <2632066077@qq.com>
2022-10-09 21:33:50 +0800

    凯撒解密算法

--- 第四次作业/Caesar算法.py
+++ 第四次作业/Caesar算法.py
@@ -10,6 +10,18 @@ for word in s:
 	s_encrypt += word_encrypt
 print(s_encrypt)
 
+#caesar算法解密
+s = input('请输入要解密的字符串：')
+k = int(input('请输入移位值：'))
+s_decrypt = ''
+for word in s:
+	if word == ' ':
+		word_decrypt = ' '
+	else:
+		word_decrypt = chr((ord(word)-ord('a') - k) % 26 + ord('a'))
+	s_decrypt += word_decrypt
+print(s_decrypt)
+
 
 
 
