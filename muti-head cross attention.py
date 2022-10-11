c8cfe6992c774aca2fc9578abacb16461b8f1d03
3120005008 <2632066077@qq.com>
2022-10-11 15:31:42 +0800

    muti_head cross attention

---
+++ muti-head cross attention.py
@@ -0,0 +1,85 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+def self_attention(query, key, value):
+    """
+    自注意力计算
+    :param query: Q
+    :param key: K
+    :param value: V
+    :param dropout: drop比率
+    :param mask: 是否mask
+    :return: 经自注意力机制计算后的值
+    """
+    d_k = query.size(-1)  # 防止softmax未来求梯度消失时的d_k
+    # Q,K相似度计算公式
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)  # Q,K相似度计算
+
+    self_attn_softmax = F.softmax(scores, dim=-1)  # 进行softmax
+
+    # 注意：返回经自注意力计算后的值，以及进行softmax后的相似度（即相似概率分布）
+    return torch.matmul(self_attn_softmax, value), self_attn_softmax
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    多头注意力计算
+    """
+
+    def __init__(self, head, d_model):
+        """
+        :param head: 头数 默认为 8
+        :param d_model: 输入的维度，必须是head的整数倍
+        """
+        super(MultiHeadAttention, self).__init__()
+        assert (d_model % head == 0)  # 确保输入维度是头数的整数倍
+        self.d_k = d_model // head  # 被拆分为多头后的某一头词向量的维度
+        self.head = head
+        self.d_model = d_model
+
+        """
+        由于多头注意力机制是针对多组Q、K、V，因此有了下面这四行代码，具体作用是，
+        针对未来每一次输入的Q、K、V，都给予参数进行构建
+        其中linear_out是针对多头汇总时给予的参数
+        """
+        self.linear_query = nn.Linear(d_model, d_model)  # 进行一个普通的全连接层变化，但不修改维度
+        self.linear_key = nn.Linear(d_model, d_model)
+        self.linear_value = nn.Linear(d_model, d_model)
+
+        self.linear_out = nn.Linear(d_model, d_model)
+
+        self.attn_softmax = None  #softmax(QK^T)
+
+    def forward(self, query, key, value):
+
+        n_batch = query.size(0)  # batch_size大小，假设query的维度是：[10, 32, 512]，其中10是batch_size的大小
+        """
+        下列三行代码都在做类似的事情，对Q、K、V三个矩阵做处理
+        其中view函数是对Linear层的输出做一个形状的重构，其中-1是自适应（自主计算）
+        从这种重构中，可以看出，虽然增加了头数，但是数据的总维度是没有变化的，也就是说多头是对数据内部进行了一次拆分
+        transopose(1,2)是对前形状的两个维度(索引从0开始)做一个交换，例如(2,3,4,5)会变成(2,4,3,5)
+        因此通过transpose可以让view的第二维度参数变成n_head
+        假设Linear成的输出维度是：[10, 32, 512]，其中10是batch_size的大小
+        注：这里解释了为什么d_model // head == d_k，如若不是，则view函数做形状重构的时候会出现异常
+        """
+        query = self.linear_query(query).view(n_batch, self.head, -1, self.d_k).transpose(1, 2) 
+        key = self.linear_key(key).view(n_batch, self.head, -1, self.d_k).transpose(1, 2)
+        value = self.linear_value(value).view(n_batch, self.head, -1, self.d_k).transpose(1, 2)
+
+        # x是通过自注意力机制计算出来的值， self.attn_softmax是相似概率分布
+        x, self.attn_softmax = self_attention(query, key, value)
+        """
+        下面的代码是汇总各个头的信息，拼接后形成一个新的x
+        其中self.head * self.d_k，可以看出x的形状是按照head数拼接成了一个大矩阵，然后输入到linear_out层添加参数
+        contiguous()是重新开辟一块内存后存储x，然后才可以使用.view方法，否则直接使用.view方法会报错
+        """
+        x = x.transpose(1, 2).contiguous().view(n_batch, -1, self.head * self.d_k)
+        return self.linear_out(x)
+
+
+
+
